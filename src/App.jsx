3640533a0342fe97d235350f5de9a01fc382a5a8
Sagar Bawanthade <sagar.bawanthade2004@gmail.com>
2025-01-17 15:06:22 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-2xl text-center justify-center text-red-700'> Real time code Editor</h1>
+      <p className='text-2xl text-center justify-center text-black'>Sagar</p>
+      <p className='text-2xl text-center justify-center text-black'>Bhumika</p>
+      <p className='text-2xl text-center justify-center text-black'>Shreya</p>
+    </>
+  )
+}
+
+export default App
